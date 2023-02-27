0dc29e096c92710c836f4fb67d57ea63adb1f9df
Christopher Álvarez <christopherjavier1998@gmail.com>
2023-02-27 11:02:11 +0000

    primeros pasos

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add("heart");
+  setTimeout(() => {
+    document.querySelector(".card").classList.remove("heart");
+    document.querySelector(".card").classList.add("spade");
+  }, 1000);
 };
