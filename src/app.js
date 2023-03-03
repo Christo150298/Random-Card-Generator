8ca57ad2910e74904319dd23e5d5edb904794164
Christopher Álvarez <christopherjavier1998@gmail.com>
2023-03-03 09:43:48 +0000

    Project finalizado

--- src/app.js
+++ src/app.js
@@ -5,17 +5,55 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let randomCard = () => {
-  let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-  let randomIndex = Math.floor(Math.random() * values.length);
-  console.log(values[randomIndex]);
-  return randomIndex;
-};
-let aleatorio = () => {
-  const selector = document.querySelector("#number");
-  selector.innerHTML = randomCard();
-};
-
 window.onload = () => {
-  setInterval(aleatorio, 2500);
+  let values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let icons = ["♦", "♥", "♠", "♣"];
+
+  document.querySelector(".card").innerHTML = `<div id='card' > 
+  <div id="icono1">${icons[0]}</div>
+  <div id="valor">${values[0]}</div>
+  <div id="icono2">${icons[0]}</div>
+  </div>`;
+
+  const createCard = () => {
+    const randomCard = () => {
+      const RamdomIcon = Math.floor(Math.random() * icons.length);
+      const RandomValue = Math.floor(Math.random() * values.length);
+      const icon = icons[RamdomIcon];
+      const value = values[RandomValue];
+      card.innerHTML = `
+      <div id="icono1">${icon}</div>
+      <div id="valor">${value}</div>
+      <div id="icono2">${icon}</div>
+      </div>`;
+      if (icon === "♥") {
+        card.innerHTML = `<div class='card-hearts red'> 
+        <div id="icono1" >${icon}</div>
+        <div id="valor" >${value}</div>
+        <div id="icono2" >${icon}</div></div>`;
+      } else if (icon === "♦") {
+        card.innerHTML = `<div class='card-Diamonds red'> 
+      <div id="icono1" >${icon}</div>
+      <div id="valor" >${value}</div>
+      <div id="icono2" >${icon}</div></div>`;
+      } else if (icon === "♠") {
+        card.innerHTML = `<div class='card-spades'> 
+        <div id="icono1" >${icon}</div>
+        <div id="valor" >${value}</div>
+        <div id="icono2" >${icon}</div></div>`;
+      } else {
+        card.innerHTML = `<div class='card-clubs'> 
+        <div id="icono1" >${icon}</div>
+        <div id="valor" >${value}</div>
+        <div id="icono2" >${icon}</div></div>`;
+      }
+    };
+    randomCard();
+  };
+
+  createCard();
+  const button = document.querySelector("#button");
+  button.addEventListener("click", createCard);
+
+  setInterval(createCard, 2000);
 };
