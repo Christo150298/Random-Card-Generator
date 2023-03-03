554cb98ac102db774136bfd9379c4352849fd7b0
Christopher Álvarez <christopherjavier1998@gmail.com>
2023-03-03 07:46:55 +0000

    ultimos pasos

--- src/app.js
+++ src/app.js
@@ -5,13 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let randomCard = () => {
+  let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let randomIndex = Math.floor(Math.random() * values.length);
+  console.log(values[randomIndex]);
+  return randomIndex;
+};
+let aleatorio = () => {
+  const selector = document.querySelector("#number");
+  selector.innerHTML = randomCard();
+};
+
 window.onload = () => {
-  document.getElementById("number");
-  let randomCard = () => {
-    let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-    let randomIndex = Math.floor(Math.random() * values.length);
-    console.log(values[randomIndex]);
-  };
-  let intervalo = setInterval(randomCard, 2500);
-  clearInterval(intervalo);
+  setInterval(aleatorio, 2500);
 };
